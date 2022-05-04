abbe50c9c817f91e8fa34a4d487185c8332e54a2
Anurag Kumar Singh <86841935+Anuragkr07@users.noreply.github.com>
2022-05-04 09:28:50 +0530

    Create Find the Duplicate Number.cpp

---
+++ Find the Duplicate Number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        unordered_set<int>s;
+        for(auto i:nums)
+        {
+            if(s.find(i)==s.end())
+            s.insert(i);
+            else
+                return i;
+        }
+        return 0;
+    }
+};
